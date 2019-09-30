84baa5ea29f76338c8ffdc1ae315390d739f32aa
Ryan C Phillips <rcphill2@wolftech.ad.ncsu.edu>
2019-09-30 17:18:48 -0400

    Worked on problem

--- code/nutrient-inputs.R
+++ code/nutrient-inputs.R
@@ -22,16 +22,75 @@ library(tidyverse)
 # Read & inspect the dataset ----
 nut <- read_csv("data/nutrient-inputs-reformatted.csv")
 
+str(nut)
+summary(nut)
+dim(nut)
+
+
 ## Tidy data ----
 # 1. Gather and separate columns
 
+nut %>%
+  gather(-State, -County, -FIPS, -Area,
+         key = "year_nutrient_application",
+         value = "kg") %>%
+  separate(year_nutrient_application,
+           into = c("year", "nutrient", "application"),
+           sep = "_") -> nut
+head(nut)
+
 # 2. Convert year to numeric
 
+as.numeric(nut$year)
+nut$year <- as.numeric(nut$year)
+str(nut)
+head(nut)
+
 # 3. Handle NAs
+nut
+## drop_na()
+nut %>%
+  drop_na()
+nut %>%
+  drop_na(kg)
+## fill()
+nut %>%
+  fill(kg, .direction = "down")
+
+## replace_na()
+nut %>%
+  replace_na(list(kg = 0))
 
 # 4. Create a NC subset
-  
+nut %>%
+  filter(State == "NC") -> nc
+head(nc)
+
 # Visualize ----
+nc %>%
+  filter(County == "WAKE") %>%
+  ggplot(mapping = aes(x = year, y = kg, color = application))+
+           geom_point() +
+           geom_line()+
+           theme_minimal() +
+  facet_wrap(~ nutrient, ncol = 1,
+             scales = "free_y")
+  
 
 # Summarize ----
 ### Which NC counties, on average over the period of record, have the highest rates of N and P input relative to different application types?
+
+nc %>%
+  group_by(County, nutrient, application) %>%
+  summarize(mean_kg = mean(kg)) %>%
+  ungroup() %>%
+  group_by(nutrient, application) %>%
+  mutate(rank = min_rank(desc(mean_kg))) %>%
+  arrange(rank)
+
+
+
+
+
+
+
